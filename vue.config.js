046ea169e690bd81009786c56acffbd81e97a062
strizich <strizich@me.com>
2018-10-05 12:34:15 -0500

    fixes for ios pwa

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,16 @@ module.exports = {
   runtimeCompiler: undefined,
   productionSourceMap: undefined,
   parallel: undefined,
-  css: undefined
+  css: undefined,
+
+  pwa: {
+    name: 'strizich.design',
+    appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      msTileImage: 'img/icons/mstile-310x310'
+    },
+    themeColor: '#000000'
+  }
 }
